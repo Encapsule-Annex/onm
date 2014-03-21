7dceec87c5898d9c43f7373cb4ab6b124594727c
Chris Russell <chrisrus@encapsule.org>
2014-03-21 12:19:48 -0700

    onm v0.1.18 updated module exports...
    ... I originally ommitted onm.Address and onm.Namespace objects from the onm module exports table as these objects are not typically instantiated by client code using the JavaScript new operator. But, we need to exports the objects so that we can write tests to verify the identity of these types in client code that derives from onm.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 module.exports.Model = require('./dist/node/ONMjs-core-model');
 module.exports.Store = require('./dist/node/ONMjs-core-store');
+module.exports.Address = require('./dist/node/ONMjs-core-address');
 module.exports.AddressStore = require('./dist/node/ONMjs-core-address-store');
+module.exports.Namespace = require('./dist/node/ONMjs-core-namespace');
 module.exports.BackChannel = require('./dist/node/encapsule-lib-backchannel');
 module.exports.util = require('./dist/node/encapsule-lib-javascript');
 
