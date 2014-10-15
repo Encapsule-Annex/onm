6548c1137ecd3b05526dba9c4a32a4f94f26eb8e
Chris Russell <chrisrus@encapsule.org>
2014-10-14 20:48:15 -0700

    Stash work on onm.Store.injectComponent.

---
+++ test/test-onm.Store.injectComponent.js
@@ -0,0 +1,38 @@
+// test-onm.Store.injectComponent.js
+//
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should;
+
+var onm = require('../onm');
+
+var testData = require('./fixture/test-data');
+
+module.exports = describe("onm.Store.injectComponent method tests.", function() {
+
+    var model = null;
+    var store1 = null, store2 = null;
+    var namespaceContact = null;
+    var addressContacts = null;
+    before(function() {
+        model = testData.createModel();
+        store1 = testData.createStore();
+        store2 = testData.createStore();
+        addressContacts = model.createPathAddress("addressBook.contacts");
+        var addressNewContact = addressContacts.createSubpathAddress("contact");
+        namespaceContact = store1.createComponent(addressNewContact);
+        assert.instanceOf(model, onm.Model);
+        assert.instanceOf(store1, onm.Store);
+        assert.instanceOf(store2, onm.Store);
+        assert.instanceOf(namespaceContact, onm.Namespace);
+    });
+
+    it("We should be able to inject a copy of the contact from store1 into store2.", function() {
+
+        assert.instanceOf(store2.injectComponent(addressContacts, namespaceContact), onm.Namespace);
+
+    });
+
+});
+
--- test/test-onm.Store.js
+++ test/test-onm.Store.js
@@ -3,5 +3,6 @@
 
 module.exports = describe("onm.Store object method tests", function() {
     require('./test-onm.Store.openNamespace');
+    require('./test-onm.Store.injectComponent');
 });
 
