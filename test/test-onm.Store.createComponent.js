bb1f6b16e60d78e5c5ba334d3a91a1637864ec5e
Chris Russell <chris@azuqua.com>
2014-10-25 08:03:23 -0700

    Update expected results JSON for test-onm.Store.createComponent...
    ... order that properties are initialized has changed which impacts the serialized JSON. Logically, the objects are identical however.

--- test/test-onm.Store.createComponent.js
+++ test/test-onm.Store.createComponent.js
@@ -168,7 +168,7 @@ module.exports = describe("onm.Store.createComponent method tests", function() {
 
         describe("Serialize the test data store to JSON and compare the results against a known good snapshot.", function() {
 
-            var expectedJSON = '{"addressBook":{"properties":{"name":"","description":"","subproperties":{"collection":{}}},"contacts":{"1":{"firstName":"","lastName":"","key":"1","emails":{},"addresses":{},"phoneNumbers":{}},"2":{"firstName":"Joe","lastName":"Smith","key":"2","emails":{},"addresses":{},"phoneNumbers":{"3":{"areaCode":"000","number":"123-4567","key":"3","notes":{"text":"This is a note assigned via a hierarchical component construction options object."}}}},"test":{"firstName":"","lastName":"","key":"test","emails":{},"addresses":{},"phoneNumbers":{}},"JoeSmith":{"firstName":"","lastName":"","key":"JoeSmith","emails":{"primary":{"key":"primary"}},"addresses":{},"phoneNumbers":{}}}}}';
+            var expectedJSON = '{"addressBook":{"properties":{"name":"","description":"","subproperties":{"collection":{}}},"contacts":{"1":{"key":"1","firstName":"","lastName":"","emails":{},"addresses":{},"phoneNumbers":{}},"2":{"key":"2","firstName":"Joe","lastName":"Smith","emails":{},"addresses":{},"phoneNumbers":{"3":{"key":"3","areaCode":"000","number":"123-4567","notes":{"text":"This is a note assigned via a hierarchical component construction options object."}}}},"test":{"key":"test","firstName":"","lastName":"","emails":{},"addresses":{},"phoneNumbers":{}},"JoeSmith":{"key":"JoeSmith","firstName":"","lastName":"","emails":{"primary":{"key":"primary"}},"addresses":{},"phoneNumbers":{}}}}}';
 
             var actualJSON = null;
             before(function() {
