a9d79e0e9a34bf3ad4120889f8aa83f8a5b8d4c4
Chris Russell <chrisrus@encapsule.org>
2014-11-05 23:32:40 -0800

    More work on the onm data model generic acceptance test suite...
    ... Okay, this is pretty cool. I've extended the dynamic tests to now dynamically enumerate an onm data model, identify all creatable subcomponents (supports nesting and recursively defined components), and then wraps each in a dynamically-generated test suite that creates the component (respecting nesting), and verifies that the component can be created w/out error. This required that I figure out how to add Mocha runnable hooks to a dynamically-created test suite container. Seems to work pretty well although it's not at all obvious how to do this sort of thing with diving into the Mocha sources.

--- test/fixture/test-shared-onmd-generic-suite.js
+++ test/fixture/test-shared-onmd-generic-suite.js
@@ -13,6 +13,8 @@ var onm = require('../../onm');
 var Mocha = require('mocha');
 var Suite = Mocha.Suite;
 var Test = Mocha.Test;
+var Hook = Mocha.Hook;
+
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 var should = require('chai').should;
@@ -25,7 +27,7 @@ module.exports = function (dataModelDeclaration_) {
 
     var dataModelDeclaration = dataModelDeclaration_;
 
-    describe("Object Namespace Manager (onm) data model declaration validator.", function() {
+    describe("Object Namespace Manager (onm) data model declaration generic acceptance test suite.", function() {
 
         describe("Base level test input data acceptance tests.", function() {
             it("The input data model declaration should be defined.", function() {
@@ -56,7 +58,7 @@ module.exports = function (dataModelDeclaration_) {
                 assert.instanceOf(onmModel, onm.Model);
             });
 
-            var dynamicDataModelDeclSuite = describe("In-depth data model validation tests.", function() {
+            var dynamicDataModelDeclSuite = describe("Data model declaration validation suite.", function() {
 
                 before(function(dynamicDataModelDeclSuiteDone_) {
 
@@ -127,7 +129,8 @@ module.exports = function (dataModelDeclaration_) {
                             if (objectModelDescriptor.namespaceType === "component") {
                                 (function() {
                                     var objectModelDescriptor_ = objectModelDescriptor;
-                                    suiteB.addTest(new Test("'" + onmModel.jsonTag + "' component namespace path '" + objectModelDescriptor_.path + "' key property declaration test.", function() {
+                                    var testTitle = "'" + onmModel.jsonTag + "' component namespace path '" + objectModelDescriptor_.path + "' key property declaration test.";
+                                    suiteB.addTest(new Test(testTitle, function() {
 
                                         var propertiesDecl = objectModelDescriptor_.namespaceModelPropertiesDeclaration;
                                         assert.isDefined(propertiesDecl);
@@ -175,26 +178,107 @@ module.exports = function (dataModelDeclaration_) {
 
             });
 
+            describe("Data model binding validation test suite.", function() {
 
-            describe("Construct an onm.Store instance bound to the newly constructed onm.Model instance.", function() {
                 var onmStore = null;
+
                 before(function() {
                     onmStore = new onm.Store(onmModel);
                 });
 
-                it("Result should be defined.", function() {
+                it("onm.Store instance bound to onm.Model should be defined.", function() {
                     assert.isDefined(onmStore);
                 });
-                it("Result should not be null.", function() {
+
+                it("onm.Store instance bound to onm.Model should not be null.", function() {
                     assert.isNotNull(onmStore);
                 });
-                it("Result should be an instance of onm.Store.", function() {
+
+                it("onm.Store instance reference should be a verified instance of onm.Store.", function() {
                     assert.instanceOf(onmStore, onm.Store);
                 });
 
+                var dynamicDataModelBindingSuite = describe("Data component construction tests.", function() {
+
+                    before(function(dynamicDataModelBindingSuiteDone_) {
+
+                        var extensionPointModelPathCoverageMap = {};
+
+                        var componentAddresses = [];
+
+                        if (onmModel.implementation.countExtensionPoints) {
+
+                            var currentComponentAddress = onmModel.createRootAddress();
+
+                            var generateSubcomponentCreateSuite = function (addressComponent_, parentTestSuite_) {
+
+                                var unresolvedSubcomponentAddresses = [];
+                                addressComponent_.visitExtensionPointAddresses(function (addressExtensionPoint_) {
+                                    var modelPath = addressExtensionPoint_.implementation.getModelPath();
+                                    if (extensionPointModelPathCoverageMap[modelPath] === undefined) {
+                                        extensionPointModelPathCoverageMap[modelPath] = true;
+                                        var unresolvedSubcomponentAddress = addressExtensionPoint_.createSubcomponentAddress();
+                                        unresolvedSubcomponentAddresses.push(unresolvedSubcomponentAddress);
+                                        // console.log("Discovered '" + unresolvedSubcomponentAddress.getHumanReadableString() + "'.");
+                                    }
+                                }); // end visit extension points
+                                // console.log("Post subcomponent discovery there are " + unresolvedSubcomponentAddresses.length + " unresolved address(es) to wrap in test suite(s).");
+
+                                unresolvedSubcomponentAddresses.forEach(function(element, index, array) {
+
+                                    // console.log("In foreach on index = " + index);
+
+                                    var unresolvedComponentAddress = element;
+                                    var componentNamespace = null;
+
+                                    var dynamicComponentCreateSuite = Suite.create(parentTestSuite_, "Component '" + element.getHumanReadableString() + "' create test.");
+                                    
+                                    var title = '"before all" hook' + dynamicComponentCreateSuite.title;
+
+                                    var fn = function(dynamicComponentCreateSuiteDone_) {
+                                        componentNamespace = onmStore.createComponent(unresolvedComponentAddress);
+                                        dynamicComponentCreateSuite.addTest(new Test("Component '" + componentNamespace.getResolvedAddress().getHumanReadableString() + "' was created.", function() {
+                                            assert.isDefined(componentNamespace);
+                                            assert.isNotNull(componentNamespace);
+                                            assert.instanceOf(componentNamespace, onm.Namespace);
+                                            // RECURSION (note that recursive data models are cycle-busted by this algorithm).
+                                            generateSubcomponentCreateSuite(componentNamespace.getResolvedAddress(), dynamicComponentCreateSuite);
+                                        }));
+                                        // console.log("Finished executing 'beforeAll' for '" + unresolvedComponentAddress.getHumanReadableString() + "'.");
+                                        dynamicComponentCreateSuiteDone_.call(dynamicComponentCreateSuite);
+                                    };
+
+                                    var hook = new Hook(title, fn);
+
+                                    hook.parent = dynamicComponentCreateSuite
+                                    hook.timeout(dynamicComponentCreateSuite.timeout());
+                                    hook.enableTimeouts(dynamicComponentCreateSuite.enableTimeouts());
+                                    hook.slow(dynamicComponentCreateSuite.slow());
+                                    hook.ctx = dynamicComponentCreateSuite.ctx;
+                                    dynamicComponentCreateSuite._beforeAll.push(hook);
+                                    dynamicComponentCreateSuite.emit('beforeAll', hook);
+
+                                    dynamicComponentCreateSuite.addTest(new Test("Execute the dynamic component create suite.", function() {
+                                        assert.isTrue(true);
+                                    }));
+
+                                });
+
+                            }; // generateSubcomponentCreateSuite
+
+                            generateSubcomponentCreateSuite(currentComponentAddress, dynamicDataModelBindingSuite);
+
+                        }
+
+                        dynamicDataModelBindingSuiteDone_();
+                    });
+
+                    it("Execute the dynamic onm data model binding test suite.", function() {
+                        assert.isTrue(true);
+                    });
+
+                });
             });
-
-
         });
     });
 };
