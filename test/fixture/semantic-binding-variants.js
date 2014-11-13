55ce126bf57f83d21e53dc4c25ace7d519af5f63
Chris Russell <chris@azuqua.com>
2014-11-13 10:13:10 -0800

    Stash onm test work from kestrel macbook.

---
+++ test/fixture/semantic-binding-variants.js
@@ -0,0 +1,74 @@
+// semantic-binding-variants.js
+//
+// This module defines a static vector of different onm data model
+// semanticBindings objects that are spliced into the 'SemanticBindingTest'
+// data model to create a vector of onm data models for testing.
+
+var semanticBindingVariants = {
+
+    { "Null component key generator.", [
+        {
+        }
+    ] },
+
+    { "With 'keyPropertyName' declaration set to some random value.", [
+        {
+        }
+    ] },
+
+    { "Internal LUID component key generator. 'keyPropertyName' === undefined.", [
+        {
+        }
+    ] },
+
+    { "Internal LUID component key generator. 'keyPropertyName' === 'key'.", [
+        {
+        }
+    ] },
+
+    { "Internal LUID component key generator. 'keyPropertyName' === 'error'.", [
+        {
+        }
+    ] },
+
+    { "Internal UUID component key generator. 'keyPropertyName' === undefined.", [
+        {
+        }
+    ] },
+
+    { "Internal UUID component key generator. 'keyPropertyName' === 'key'.", [
+        {
+        }
+    ] },
+
+    { "Internal UUID component key generator. 'keyPropertyName' === 'error'.", [
+        {
+        }
+    ] },
+
+
+
+
+
+    { "Internal UUID component key generator. 'keyPropertyName' === undefined.", [
+        {
+        }
+    ] },
+
+    { "Internal UUID component key generator. 'keyPropertyName' === undefined.", [
+        {
+        }
+    ] },
+
+
+    
+
+
+
+    { "External developer-defined component key generator test.", [
+        {
+        }
+    ] }
+
+};
+
