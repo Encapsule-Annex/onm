d035b62b0945e8b2c8ffcd5fefc8415a3ee9ee30
Chris Russell <chris@azuqua.com>
2014-12-09 17:05:32 -0800

    Refactor descriptor resolve create test into generic suite.

--- test/test-impl-onm-descriptor-resolve.create.js
+++ test/test-impl-onm-descriptor-resolve.create.js
@@ -11,66 +11,84 @@ var onm = require('../index');
 var testData = require('./fixture/address-book-data-model');
 
 var testDataModel = testData.createModel();
-var testDataRootAddress = testDataModel.createRootAddress();
-var testDataRootToken = testDataRootAddress.implementation.getLastToken();
-var testDataRootDescriptor = testDataRootToken.namespaceDescriptor;
+var semanticBindingsObject = testDataModel.getSemanticBindings();
+
+var rootAddress = testDataModel.createRootAddress();
+var rootDescriptor = rootAddress.implementation.getDescriptor();
+var childDescriptor = rootAddress.implementation.getModelDescriptorFromSubpath("properties");
+var extensionPointDescriptor = rootAddress.implementation.getModelDescriptorFromSubpath("contacts");
+var componentDescriptor = rootAddress.implementation.getModelDescriptorFromSubpath("contacts.contact");
 
 var moduleUnderTest = require('../lib/implementation/onm-descriptor-resolve');
 
+// Test dimensions for resolve descriptor create
+// - namespace type: root, child, extension point, component
+// - target namespace key: undefined, defined, mismatch
+// - property assignment: undefined, prop subset, prop superset, prop disjoint set simple, prop disjoint set compount
+// - subobject assignment: undefined, child, extension point, component
+
+
+
+var testDataVector = {
+
+    'test': [{
+        options: {
+            parentDataReference: {},
+            targetNamespaceDescriptor: rootDescriptor,
+            targetNamespaceKey: '',
+            propertyAssignmentObject: {},
+            semanticBindingsReference: semanticBindingsObject
+        },
+        validConfig: true
+    }]
+};
+
+
+
 module.exports = describe("'resolveNamespaceDescriptorCreate' function export tests.", function() {
 
-    var resolveResults = null;
-    var descriptorResolveOptions = {
-        parentDataReference: {},
-        targetNamespaceDescriptor: testDataRootDescriptor,
-        targetNamespaceKey: '',
-        propertyAssignmentObject: {
-            key: 'testkey', 
-            name: "Does he look like a bitch?",
-            properties: {
-                name: 'What does Marsellus Wallace look like?'
-            },
-            someRandomObject: {
-                someProperty: 'this is a test',
-                subobject: {
-                    someProperty: 'this is another test'
-                }
-            }
-        },
-        semanticBindingsReference: testDataModel.getSemanticBindings()
-    };
-
     before(function(done_) {
-        var functionUnderTest = function() {
-            resolveResults = moduleUnderTest.resolveNamespaceDescriptorCreate(descriptorResolveOptions);
-        };
-        assert.doesNotThrow(functionUnderTest);
+
+        withData(testDataVector, function(testData) {
+
+            var testName = "Attempt to resolve the namespace descriptor '" +
+                testData.options.targetNamespaceDescriptor.jsonTag + "' of type '" +
+                testData.options.targetNamespaceDescriptor.namespaceType + "'.";
+
+            describe(testName, function() {
+                var resolveResults = null;
+                before(function(done_) {
+                    var functionUnderTest = function() {
+                        resolveResults = moduleUnderTest.resolveNamespaceDescriptorCreate(testData.options);
+                    };
+                    assert.doesNotThrow(functionUnderTest);
+                });
+                it("Function call should have returned an object.", function() {
+                    assert.isDefined(resolveResults);
+                    assert.isNotNull(resolveResults);
+                    assert.isObject(resolveResults);
+                });
+                it("The returned object should be a valid descriptor resolve results object.", function() {
+                    assert.isTrue(moduleUnderTest.checkValidDescriptorResolveResults(resolveResults));
+                });
+                it("Verify that named child object 'addressBook' was created in the parent store object.", function() {
+                    assert.property(testData.options.parentDataReference, 'addressBook');
+                    assert.isObject(testData.options.parentDataReference.addressBook);
+                });
+                it("Verify the integrity of the resolved child object 'addressBook' data reference.", function() {
+                    assert.doesNotThrow(function() {
+                        resolveResults.namespaceDataReference.test = "test property touched";
+                    });
+                    assert.property(testData.options.parentDataReference.addressBook, 'test');
+                    assert.equal(testData.options.parentDataReference.addressBook.test, "test property touched");
+                });
+                it("Execute the test.", function() {
+                    assert.isTrue(true);
+                });                    
+            });
+        });
         done_();
     });
-
-    it("Function call should have returned an object.", function() {
-        assert.isDefined(resolveResults);
-        assert.isNotNull(resolveResults);
-        assert.isObject(resolveResults);
-    });
-
-    it("The returned object should be a valid descriptor resolve results object.", function() {
-        assert.isTrue(moduleUnderTest.checkValidDescriptorResolveResults(resolveResults));
-    });
-
-    it("Verify that named child object 'addressBook' was created in the parent store object.", function() {
-        assert.property(descriptorResolveOptions.parentDataReference, 'addressBook');
-        assert.isObject(descriptorResolveOptions.parentDataReference.addressBook);
-    });
-
-    it("Verify the integrity of the resolved child object 'addressBook' data reference.", function() {
-        assert.doesNotThrow(function() {
-            resolveResults.namespaceDataReference.test = "test property touched";
-        });
-        assert.property(descriptorResolveOptions.parentDataReference.addressBook, 'test');
-        assert.equal(descriptorResolveOptions.parentDataReference.addressBook.test, "test property touched");
-    });
-
 });
 
 
