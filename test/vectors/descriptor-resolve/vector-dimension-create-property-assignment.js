61c121e89f5987ac6d2bd841477937388d99f551
Chris Russell <chrisrus@encapsule.org>
2014-12-12 13:12:08 -0800

    More test cases for property assignemnt object.

--- test/vectors/descriptor-resolve/vector-dimension-create-property-assignment.js
+++ test/vectors/descriptor-resolve/vector-dimension-create-property-assignment.js
@@ -10,49 +10,54 @@ module.exports = {
             data: {},
             validConfig: true
         },
-        /*
+
         {
             label: 'declared properties subset',
             data: {
-                contacts: {
-                    testKey: {
-                        name: "set via properties object"
+                a: 'blue',
+                b: 'blue'
+            },
+            validConfig: true
+        },
+
+        {
+            label: 'undeclared properties set',
+            data: {
+                _a: 'red',
+                _b: 'red'
+            },
+            validConfig: true
+        },
+
+        {
+            label: 'declared properties superset',
+            data: {
+                a: 'blue',
+                b: 'blue',
+                c: 'blue',
+                d: 'blue',
+                e: 'blue',
+                f: 'blue',
+                _a: 'red',
+                _b: 'red'
+            },
+            validConfig: true
+        },
+
+        {
+            label: 'undeclared subobjects set',
+            data: {
+                _a: {
+                    _b: {
+                        c_: {
+                            _d: 'red'
+                        }
                     }
                 }
             },
             validConfig: true
-        },
-        {
-            label: 'declared properties superset',
-            data: {
-            },
-            validConfig: true
-        },
-        {
-            label: 'undeclared properties set',
-            data: {
-            },
-            validConfig: true
-        },
-        {
-            label: 'undeclared subobjects set',
-            data: {
-            },
-            validConfig: true
-        },
-        {
-            label: 'declared subobject set',
-            data: {
-            },
-            validConfig: true
-        },
-        {
-            label: 'mix of declared/undeclared props/objects',
-            data: {
-            },
-            validConfig: true
         }
-        */
+
     ]
 };
 
