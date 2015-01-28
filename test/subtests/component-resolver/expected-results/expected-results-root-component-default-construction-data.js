0bfa650b7a8db32afe35deee4aa09b69bc7adfac
Chris Russell <chrisrus@encapsule.org>
2015-01-27 16:55:17 -0800

    Start bucketing the results right away. We don't want to... ... have to edit each use case when we refactor/fix bugs in the source under test.

---
+++ test/subtests/component-resolver/expected-results/expected-results-root-component-default-construction-data.js
@@ -0,0 +1,5 @@
+// expected-results-root-component-default-construction.js
+//
+
+module.exports = '{"namespaceRoot":{"a":"default a","b":"default b","c":"default c","d":"default d","e":"default e","f":"default f","namespaceExtensionPointA":{},"namespaceChildA":{"a":"default a","b":"default b","c":"default c","d":"default d","e":"default e","f":"default f","namespaceExtensionPointB":{},"namespaceChildB":{"a":"default a","b":"default b","c":"default c","d":"default d","e":"default e","f":"default f","namespaceChildC":{"a":"default a","b":"default b","c":"default c","d":"default d","e":"default e","f":"default f","namespaceChildD":{"a":"default a","b":"default b","c":"default c","d":"default d","e":"default e","f":"default f","namespaceExtensionPointE":{},"namespaceChildE":{"a":"default a","b":"default b","c":"default c","d":"default d","e":"default e","f":"default f"}}}}}}}';
+
