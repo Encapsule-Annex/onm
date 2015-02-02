e11230d543912388b88640a65c2fc2b3fdb7c075
Chris Russell <chrisrus@encapsule.org>
2015-02-02 13:32:14 -0800

    create-apply-1-1 reowrked.

---
+++ test/subtests/address-resolver/test-use-case-address-resolver-create-apply-1-1.js
@@ -0,0 +1,64 @@
+// test-use-case-address-resolver-create-apply-1-1.js
+//
+
+var onm = require('../../../index');
+var testAddressResolverUseCase = require('./test-core-address-resolver');
+
+var dataModelDeclaration = {
+    semanticBindings: {
+        componentKeyGenerator: 'internalLuid'
+    },
+    jsonTag: 'testData',
+    subNamespaces: [
+        {
+            namespaceType: 'extensionPoint',
+            jsonTag: 'hashtable',
+            componentArchetype: {
+                namespaceType: 'component',
+                jsonTag: 'testRecord',
+                subNamespaces: [
+                    {
+                        namespaceType: 'extensionPoint',
+                        jsonTag: 'hashtable',
+                        componentArchetypePath: 'testData.hashtable.testRecord'
+                    }
+                ]
+            }
+        }
+    ]
+};
+
+var dataModel = new onm.Model(dataModelDeclaration).implementation.resetKeyGenerator();
+var rootAddress = dataModel.createRootAddress();
+var testAddress = rootAddress.createSubpathAddress('hashtable.testRecord');
+for (var token in testAddress.implementation.tokenVector) {
+    testAddress.implementation.tokenVector[token].key = 'test';
+}
+
+testResult = testAddressResolverUseCase({
+    strategyName: "create",
+    operationName: "apply data-1",
+    targetNamespace: "subcomponent-1",
+    inputOptions: {
+        strategy: "create",
+        address: testAddress,
+        parentDataReference: {},
+        semanticBindingsReference: dataModel.getSemanticBindings(),
+        propertyAssignmentObject: {
+            testKeyValue: { test: "test data value in root namespace" }
+        }
+    },
+    expectCallToThrow: false,
+    resultExpectations: {
+        resolvedComponentCount: 2,
+        dataChangeEventJournalCount: 5,
+        JSON: {
+            namespace: '{"test":"test data value in root namespace","hashtable":{}}',
+            parent: '{"testData":{"hashtable":{"testKeyValue":{"test":"test data value in root namespace","hashtable":{}}}}}',
+            journal: '[{"layer":"namedObject","event":"namedObjectCreated","eventData":{"namespaceType":"root","namespaceModelPath":"testData","namespaceModelId":0,"key":"testData"}},{"layer":"namedObject","event":"namedObjectCreated","eventData":{"namespaceType":"extensionPoint","namespaceModelPath":"testData.hashtable","namespaceModelId":1,"key":"hashtable"}},{"layer":"namedObject","event":"namedObjectCreated","eventData":{"namespaceType":"component","namespaceModelPath":"testData.hashtable.testRecord","namespaceModelId":2,"key":"testKeyValue"}},{"layer":"namedObject","event":"propertyInitialized","eventData":{"name":"test","model":false,"value":"\\"test data value in root namespace\\"","source":"data"}},{"layer":"namedObject","event":"namedObjectCreated","eventData":{"namespaceType":"extensionPoint","namespaceModelPath":"testData.hashtable.testRecord.hashtable","namespaceModelId":3,"key":"hashtable"}}]'
+        }
+    }
+});
+
+
+
